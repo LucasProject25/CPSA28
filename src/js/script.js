f10431c4543dd6683d26d47418b4a289914f4300
Lucas Fulcrand <fulcrandlucas@gmail.com>
2025-12-03 09:50:02 +0100

    Dépot du projet sur git

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+const nav = document.querySelector(".menu");
+const toggle = document.querySelector(".menu-btn");
+const page = document.body;
+
+toggle.addEventListener("click", () => {
+    const isOpen = toggle.ariaExpanded === "true";
+    const isClosed = !isOpen;
+
+    toggle.ariaExpanded = isClosed;
+    nav.ariaHidden = isOpen;
+    page.classList.toggle("noscroll", isClosed);
+});
